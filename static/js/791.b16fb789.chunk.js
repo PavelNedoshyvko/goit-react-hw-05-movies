fb899d01c54ceb8945a8c08a510bdc8754edec8f
PavelNedoshyvko <113437527+PavelNedoshyvko@users.noreply.github.com>
2023-11-02 18:53:09 +0000

    Deploying to gh-pages from @ PavelNedoshyvko/goit-react-hw-05-movies@0441802c813dda51235f31379e4170fc3289e61f 🚀

---
+++ static/js/791.b16fb789.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[791],{680:function(e,t,r){r.r(t),r.d(t,{default:function(){return s}});var o=r(87),n=r(184);function s(){return(0,n.jsxs)("div",{children:["Sorry, no such route exists. Please follow this link to the",(0,n.jsx)(o.rU,{to:"/",children:"home page"})]})}}}]);
+//# sourceMappingURL=791.b16fb789.chunk.js.map
