050596037208541ea0ea20a66a1824005b9285c8
Pavel Nedoshyvko <plight83@gmail.com>
2023-11-02 21:00:02 +0200

    update code splitting

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Cast } from "./Cast/Cast";
 import { Reviews } from "./Reviews/Reviews";
 import { Layout } from "./Layout/Layout";
-import { Suspense, lazy } from "react";
+import { lazy } from "react";
 
 const Home = lazy(() => import("pages/Home"));
 const MovieDetails = lazy(() => import("pages/MovieDetails"));
@@ -11,18 +11,18 @@ const NotFoundPage = lazy(() => import("pages/NotFoundPage"));
 
 export const App = () => {
 	return (
-		<Suspense fallback={'LOADING PAGE...'}>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route index element={<Home />} />
-					<Route path="movies" element={<Movies />} />
-					<Route path="movies/:movieId" element={<MovieDetails />}>
-						<Route path="cast" element={<Cast />} />
-						<Route path="reviews" element={<Reviews />} />
-					</Route>
-					<Route path="*" element={<NotFoundPage />} />
+
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<Home />} />
+				<Route path="movies" element={<Movies />} />
+				<Route path="movies/:movieId" element={<MovieDetails />}>
+					<Route path="cast" element={<Cast />} />
+					<Route path="reviews" element={<Reviews />} />
 				</Route>
-			</Routes >
-		</Suspense >
+				<Route path="*" element={<NotFoundPage />} />
+			</Route>
+		</Routes >
+
 	);
 };
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import { Container, Header, Link, Nav } from "./Layout.styled"
+import { Suspense } from "react"
 
 export const Layout = () => {
 	return (
@@ -10,7 +11,10 @@ export const Layout = () => {
 					<Link to="/movies">Movies</Link>
 				</Nav>
 			</Header>
-			<Outlet />
+			<Suspense>
+				<Outlet />
+			</Suspense>
+
 		</Container>
 
 	)
